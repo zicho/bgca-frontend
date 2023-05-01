6ce0b3316a72d75ba1cc94e425274b38cb7ae659
Martin Ström <martin@datanist.se>
2023-05-01 20:20:53 +0200

    user profile and edit added

--- src/routes/game/+page.server.ts
+++ src/routes/game/+page.server.ts
@@ -1,11 +1,4 @@
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-    console.log("load")
-
-	var data = await locals.api.client.game.getApiGame1(1);
-
-    console.dir(data)
-
-	return { data: data.data };
 };
