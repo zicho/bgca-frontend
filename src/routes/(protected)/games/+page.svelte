c1d13487ad4c14f35c980daefdfb29945499f31d
Martin Ström <martin@datanist.se>
2023-05-18 09:02:33 +0200

    progress on game viewing

---
+++ src/routes/(protected)/games/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	let { games } = data;
+</script>
+
+{#each games as game}
+	<a href="/games/{game.id}/{game.name?.toLocaleLowerCase()}">
+		<div class="row">
+			<div class="col col-lg-2 col-md-2 col-sm-3 col-xs-3">
+				<img src={game.thumbnailUrl} alt="{game.name} cover art" />
+			</div>
+			<div  class="item-content col col-lg-10 col-md-10 col-sm-9 col-xs-9">
+				<h3>{game.name}</h3>
+				{#if game.description}
+					<p><small>{game.description}</small></p>
+				{:else}
+					<p><small>No description</small></p>
+				{/if}
+			</div>
+		</div>
+	</a>
+	<hr />
+{/each}
+
+<style>
+	* {
+		padding: 0;
+		margin: 0;
+	}
+
+	a,
+	p,
+	small {
+		color: inherit;
+	}
+	a:hover {
+		text-decoration: none;
+	}
+
+    .item-content {
+        padding-left: 20px
+    }
+
+	img {
+		width: 100%;
+		aspect-ratio: 1/1;
+	}
+
+	.row {
+		margin: 20px 0px;
+	}
+
+	.row:hover {
+		background-color: ghostwhite;
+	}
+</style>
